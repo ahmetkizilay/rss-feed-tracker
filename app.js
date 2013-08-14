bd5f7679387492969ccededa838bddc43316c2a5
ahmet kizilay <ahmet.kizilay@gmail.com>
2013-08-15 00:18:52 +0300

    started using soupselect for keyword parsing

--- app.js
+++ app.js
@@ -2,11 +2,11 @@ var cronJob = require('cron').CronJob;
 var url = require('url');
 var http = require('http');
 var htmlparser = require('htmlparser');
+var select = require('soupselect').select;
 var async = require('async');
 var Buffer = require('buffer').Buffer;
 var Win1254 = require('./encoders/win1254');
 
-var jsonQuery = require('json-query');
 var mongoose = require('mongoose');
 var Feed = require('./models/Feed.js')(mongoose);
 
@@ -16,37 +16,13 @@ var cronTime = "*/5 * * * * *";
 var testUrl = "http://www.radikal.com.tr/d/rss/Rss_77.xml";
 
 var parseKeywords = function(domJson) {
-    var filter = {
-        test: function(input) {
-            var result;
-            try {
-                input.forEach(function (ddd) {
-                    
-                    if(ddd.name ==='meta' && ddd.attribs.name === 'keywords') {
-                        result = ddd.attribs.content;
-                        throw {msg: "custom error"};
-                    }
-                });
-            }
-            catch(e) {
-                if(e.msg === undefined || e.msg !== 'custom error') {
-                    throw e;
-                }
-            }
 
-            return result;
-        }
-    };
+    var keywords = [];
+    select(domJson, "div.index_keywords a h2").forEach(function (item) {
+        keywords.push(Win1254.toUTF8(item.children[0].raw));
+    });
 
-    var queryInput = {
-        tags: domJson
-    };
-
-    var queryResult = jsonQuery('tags[name=html].children[name=head].children:test', {
-        rootContext: queryInput, filters: filter
-    }).value;
-
-    console.log(Win1254.toUTF8(queryResult));
+    return keywords;
 };
 
 var feedDownloader = function(options, callbackComplete) {
@@ -64,8 +40,9 @@ var feedDownloader = function(options, callbackComplete) {
                         throw err;
                     }
 
-                    parseKeywords(dom);
-
+                    var keywords = parseKeywords(dom);
+                    console.log(keywords);
+                    
                     urlDownloadComplete(null);
                 });
             }, function (err) {
@@ -115,24 +92,19 @@ var urlDownloader = function (options, callbackComplete) {
 
     var req = http.request(options, function (res) {
         res.setEncoding('binary');
-        //var output = '';
         res.on('data', function (chunk) {
-            //output += chunk;
             urlParser.parseChunk(chunk);
         });
 
         res.on('end', function () {
             urlParser.done();
-            //var buffer = Buffer.concat(data);
-
-            //console.log(output);
         });
     });
 
     req.on('error', function (err) {
         console.log('error occurred');
         throw err;
-    })
+    });
 
     req.end();
 };
