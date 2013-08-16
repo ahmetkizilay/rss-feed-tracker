231fc3804da01ae9b09627f09f118af0b1ff5760
ahmet kizilay <ahmet.kizilay@gmail.com>
2013-08-16 23:54:19 +0300

    refactored feed download into a separate file

---
+++ FeedDownloader.js
@@ -0,0 +1,126 @@
+var FeedDownloader = function () {
+    "use strict";
+
+    var url = require('url');
+    var htmlparser = require('htmlparser');
+    var async = require('async');
+    var http = require('http');
+    var Buffer = require('buffer').Buffer;
+    var Win1254 = require('./encoders/win1254');
+    var select = require('soupselect').select;
+    var mongoose = require('mongoose');
+    var Feed = require('./models/Feed.js')(mongoose);
+
+    var _parseKeywords = function(domJson) {
+
+        var keywords = [];
+        select(domJson, "div.index_keywords a h2").forEach(function (item) {
+            keywords.push(Win1254.toUTF8(item.children[0].raw));
+        });
+
+        return keywords;
+    };
+
+    var _urlDownloader = function (options, downloadComplete) {
+        var urlHandler = new htmlparser.DefaultHandler(function (err, dom) {
+            if(err) {
+                downloadComplete(err);
+            }
+
+            console.log('finished downloading: ' + options.path);
+            downloadComplete(null, dom);
+        });
+
+        var urlParser = new htmlparser.Parser(urlHandler);
+
+        var req = http.request(options, function (res) {
+            res.setEncoding('binary');
+            res.on('data', function (chunk) {
+                urlParser.parseChunk(chunk);
+            });
+
+            res.on('end', function () {
+                urlParser.done();
+            });
+        });
+
+        req.on('error', function (err) {
+            console.log('error occurred');
+            throw err;
+        });
+
+        req.end();
+    };
+
+    /**
+    * first the rss feed is downloaded. rss handler parses individual items and
+    * downloads the referenced url sequentially. Upon each download, keywords are
+    * extracted from the link. The results are stored in a mongo Schema called Feed.
+    */
+    var _download = function (options, callback) {
+        var groupId = options.id;
+        var urlOptions = url.parse(options.url);
+
+        var rssHandler = new htmlparser.RssHandler(function (err, dom) {
+            if(err) {
+                throw err;
+            }
+
+            if(dom.items) {
+                async.eachSeries(dom.items, function (item, urlDownloadComplete) {
+                    // download file
+                    console.log('downloading: ' + item.link);
+                    _urlDownloader(url.parse(item.link), function (err, dom) {
+                        if(err) {
+                            throw err;
+                        }
+
+                        var keywords = _parseKeywords(dom);
+                        
+                        Feed.insertFeed('radikal', item.title, item.link, item.description, item.pubDate, keywords, function (err, msg) {
+                            if(err) {
+                                urlDownloadComplete(err);
+                            }
+
+                            urlDownloadComplete(null);
+                        });
+                    });
+                }, function (err) {
+                    if(err) {
+                        throw err;
+                    }
+
+                    callbackComplete();
+                });
+            }
+        });
+
+        var rssParser = new htmlparser.Parser(rssHandler);
+
+        var req = http.request(urlOptions, function (res) {
+        res.setEncoding('utf8');
+        rssParser.reset();
+
+        res.on('data', function (chunk) {
+            rssParser.parseChunk(chunk);
+        });
+
+        res.on('end', function () {
+            rssParser.done();
+        });
+    });
+
+    req.on('error', function (err) {
+        throw err;
+    });
+
+    req.end();
+
+    };
+
+    return {
+        download: _download
+    };
+}();
+
+module.exports = FeedDownloader;
--- app.js
+++ app.js
@@ -1,120 +1,21 @@
 var cronJob = require('cron').CronJob;
-var url = require('url');
-var http = require('http');
-var htmlparser = require('htmlparser');
-var select = require('soupselect').select;
 var async = require('async');
-var Buffer = require('buffer').Buffer;
-var Win1254 = require('./encoders/win1254');
+var FeedDownloader = require('./FeedDownloader');
 
 var mongoose = require('mongoose');
-var Feed = require('./models/Feed.js')(mongoose);
-
 mongoose.connect('mongodb://localhost/rssfeedtracker');
 
 var cronTime = "*/5 * * * * *";
 var testUrl = "http://www.radikal.com.tr/d/rss/Rss_77.xml";
-
-var parseKeywords = function(domJson) {
-
-    var keywords = [];
-    select(domJson, "div.index_keywords a h2").forEach(function (item) {
-        keywords.push(Win1254.toUTF8(item.children[0].raw));
-    });
-
-    return keywords;
-};
-
-var feedDownloader = function(options, callbackComplete) {
-    var rssHandler = new htmlparser.RssHandler(function (err, dom) {
-        if(err) {
-            throw err;
-        }
-
-        if(dom.items) {
-            async.eachSeries(dom.items, function (item, urlDownloadComplete) {
-                // download file
-                console.log('downloading: ' + item.link);
-                urlDownloader(url.parse(item.link), function (err, dom) {
-                    if(err) {
-                        throw err;
-                    }
-
-                    var keywords = parseKeywords(dom);
-                    console.log(keywords);
-                    
-                    urlDownloadComplete(null);
-                });
-            }, function (err) {
-                if(err) {
-                    throw err;
-                }
-
-                callbackComplete();
-            });
-        }
-    });
-
-    var rssParser = new htmlparser.Parser(rssHandler);
-
-    var req = http.request(options, function (res) {
-        res.setEncoding('utf8');
-        rssParser.reset();
-
-        res.on('data', function (chunk) {
-            rssParser.parseChunk(chunk);
-        });
-
-        res.on('end', function () {
-            rssParser.done();
-        });
-    });
-
-    req.on('error', function (err) {
-        throw err;
-    });
-
-    req.end();
-};
-
-var urlDownloader = function (options, callbackComplete) {
-    var urlHandler = new htmlparser.DefaultHandler(function (err, dom) {
-        if(err) {
-            callbackComplete(err);
-        }
-
-        // insertFeedHere
-        console.log('finished downloading: ' + options.path);
-        callbackComplete(null, dom);
-    });
-
-    var urlParser = new htmlparser.Parser(urlHandler);
-
-    var req = http.request(options, function (res) {
-        res.setEncoding('binary');
-        res.on('data', function (chunk) {
-            urlParser.parseChunk(chunk);
-        });
-
-        res.on('end', function () {
-            urlParser.done();
-        });
-    });
-
-    req.on('error', function (err) {
-        console.log('error occurred');
-        throw err;
-    });
-
-    req.end();
-};
 
 var job = new cronJob({
     cronTime: cronTime,
     onTick: function() {
         console.log("cron activated");
 
-        feedDownloader(url.parse(testUrl));
+        FeedDownloader.download({id: 'Radikal', url: testUrl}, function (err, msg) {
+            console.log(msg);
+        });
     },
     start: true,
 });
